a52b65361442dc241c1843c41155ebdb33297c5f
Lê Hà Gia Bảo <lehagiabao0205@gmail.com>
2023-11-17 13:03:11 +0700

    Add button navigation

--- src/containers/Profile/ButtonNavigation/ButtonNavigation.tsx
+++ src/containers/Profile/ButtonNavigation/ButtonNavigation.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './ButtonNavigation.css'
+import { FiHeart } from "react-icons/fi";
+import { BiMessageAltDots } from "react-icons/bi";
+import { GoBell } from "react-icons/go";
 
 function ButtonNavigation() {
     return (
         <>
             <div className='buttonNavigation'>
-
+                <div className='btnNavContainer'>
+                    <div className='btnIcon'>
+                        <FiHeart className='btnIconClass' />
+                    </div>
+                    <h1 className='btnContent'>Đã lưu</h1>
+                </div>
+                <div className='btnNavContainer'>
+                    <div className='btnIcon'>
+                        <BiMessageAltDots className='btnIconClass' />
+                    </div>
+                    <h1 className='btnContent'>Tin nhắn</h1>
+                </div>
+                <div className='btnNavContainer'>
+                    <div className='btnIcon'>
+                        <GoBell className='btnIconClass' />
+                    </div>
+                    <h1 className='btnContent'>Thông báo</h1>
+                </div>
             </div>
         </>
     );
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,12 +1,14 @@
 import { IonContent } from '@ionic/react';
 import './Profile.css';
 import ProfileNavbar from '../../containers/Profile/ProfileNavbar/ProfileNavbar';
+import ButtonNavigation from '../../containers/Profile/ButtonNavigation/ButtonNavigation';
 
 const Profile: React.FC = () => {
     return (
         <IonContent fullscreen>
             <div className='profileContainer'>
                 <ProfileNavbar />
+                <ButtonNavigation />
             </div>
         </IonContent>
     );
