3eb8d40e5e815bdce8f9dfdd87a956f6fdb610fb
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-09-29 22:25:16 +0700

    Add launch screen in iOS and Android

--- ios/Wanderlust/AppDelegate.mm
+++ ios/Wanderlust/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import "RNSplashScreen.h"
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -10,6 +10,9 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  [RNSplashScreen show]; 
+  return YES;
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
