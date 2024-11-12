c84f65dd47291286964c678232ef6215be8d7641
LeHaGiaBao <lehagiabao0205@gmail.com>
2024-11-12 16:09:03 +0700

    refactor: refactor share component

--- src/screens/tabs/Explore/index.tsx
+++ src/screens/tabs/Explore/index.tsx
@@ -1,9 +1,30 @@
 import React, {memo} from 'react';
-import {View} from 'react-native';
-import {styles} from './styles';
+import {StyleSheet, View} from 'react-native';
+import {BaseColor, Devices} from '@/constants';
+import TopNavBar from '../_components/TopNavBar';
+import translate from '@/translations/i18n';
 
 function ExploreScreen() {
-  return <View style={styles.exploreScreen} />;
+  return (
+    <View style={styles.exploreScreen}>
+      <TopNavBar
+        title={translate('source:hello')}
+        subTitle={translate('source:share_experiences')}
+        showSearchBar
+        searchBarPlaceholder={translate('source:find_experiences')}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  exploreScreen: {
+    display: 'flex',
+    paddingTop: 77,
+    paddingHorizontal: 16,
+    backgroundColor: BaseColor.White,
+    height: Devices.height,
+  },
+});
 
 export default memo(ExploreScreen);
