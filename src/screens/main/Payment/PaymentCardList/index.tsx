73bb3d4436bf868585cecbef09eadc12c8696e87
LeHaGiaBao <lehagiabao0205@gmail.com>
2025-02-22 15:55:35 +0700

    feat: add payment confirm modal

--- src/screens/main/Payment/PaymentCardList/index.tsx
+++ src/screens/main/Payment/PaymentCardList/index.tsx
@@ -1,13 +1,14 @@
-import React, {memo, useCallback} from 'react';
+import React, {memo, useCallback, useEffect, useState} from 'react';
 import {StyleSheet, TouchableOpacity, View} from 'react-native';
 import {TopNavigation} from '@/components/containers';
 import {WText} from '@/components/UIKit';
-import {BaseColor, Devices, StatusColor} from '@/constants';
+import {BaseColor, Devices, PrimaryColor, StatusColor} from '@/constants';
 import translate from '@/translations/i18n';
-import {AddCircle, Trash} from 'iconsax-react-native';
+import {AddCircle, Trash, Verify} from 'iconsax-react-native';
 import PaymentItem from './components/PaymentItem';
 import {useWanderlustNavigation} from '@/hooks/core/core';
 import {Routes} from '@/routes/routes';
+import Modal from 'react-native-modal';
 
 const VISA =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/1000px-Visa_Inc._logo.svg.png';
@@ -16,11 +17,31 @@ const MASTERCARD =
 const JCB = 'https://www.jcbusa.com/JCB-logos/EmblemColor.png';
 
 function PaymentCardList() {
+  const [isConfirmModal, setConfirmModal] = useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
   const nav = useWanderlustNavigation();
 
   const handleCard = useCallback(() => {
     nav.navigate(Routes.payment_edit);
   }, [nav]);
+
+  const toggleModal = useCallback(() => {
+    setConfirmModal(!isConfirmModal);
+    setModalVisible(false);
+  }, [isConfirmModal]);
+
+  const handleDeleteCard = useCallback(() => {
+    setModalVisible(!isModalVisible);
+  }, [isModalVisible]);
+
+  useEffect(() => {
+    if (isConfirmModal) {
+      const timer = setTimeout(() => {
+        setConfirmModal(!isConfirmModal);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [isConfirmModal]);
 
   return (
     <View style={styles.container}>
@@ -38,17 +59,38 @@ function PaymentCardList() {
           <PaymentItem
             image={VISA}
             title="**** **** **** 9090"
-            icon={<Trash size={20} variant="Bold" color={StatusColor.Error} />}
+            icon={
+              <Trash
+                size={20}
+                variant="Bold"
+                color={StatusColor.Error}
+                onPress={toggleModal}
+              />
+            }
           />
           <PaymentItem
             image={MASTERCARD}
             title="**** **** **** 1234"
-            icon={<Trash size={20} variant="Bold" color={StatusColor.Error} />}
+            icon={
+              <Trash
+                size={20}
+                variant="Bold"
+                color={StatusColor.Error}
+                onPress={toggleModal}
+              />
+            }
           />
           <PaymentItem
             image={JCB}
             title="**** **** **** 1234"
-            icon={<Trash size={20} variant="Bold" color={StatusColor.Error} />}
+            icon={
+              <Trash
+                size={20}
+                variant="Bold"
+                color={StatusColor.Error}
+                onPress={toggleModal}
+              />
+            }
           />
         </View>
 
@@ -61,6 +103,58 @@ function PaymentCardList() {
           />
         </TouchableOpacity>
       </View>
+
+      <Modal isVisible={isConfirmModal}>
+        {isModalVisible ? (
+          <View style={styles.modal}>
+            <WText
+              text={translate('source:notification')}
+              typo="Heading2"
+              color="Main"
+            />
+            <Verify size={55} color={PrimaryColor.Main} variant="Bold" />
+            <WText
+              text={translate('source:edit_card_status:delete_success')}
+              typo="Body2"
+              color="Black"
+            />
+          </View>
+        ) : (
+          <View style={styles.modal}>
+            <WText
+              text={translate('source:notification')}
+              typo="Heading2"
+              color="Main"
+            />
+            <WText
+              text={translate('source:edit_card_status:delete_confirm')}
+              typo="Body2"
+              color="Black"
+            />
+            <View style={styles.modalButton}>
+              <TouchableOpacity
+                style={styles.buttonMainStyle}
+                onPress={handleDeleteCard}>
+                <WText
+                  text={translate('source:confirm')}
+                  typo="Body2"
+                  color="White"
+                />
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.buttonErrorStyle}
+                onPress={toggleModal}>
+                <WText
+                  text={translate('source:cancel')}
+                  typo="Body2"
+                  color="Error"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      </Modal>
     </View>
   );
 }
@@ -93,6 +187,41 @@ const styles = StyleSheet.create({
     marginVertical: 16,
     display: 'flex',
     gap: 8,
+  },
+  modal: {
+    borderRadius: 8,
+    padding: 32,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: BaseColor.White,
+    gap: 24,
+    height: 200,
+  },
+  modalButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 16,
+  },
+  buttonMainStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 40,
+    borderRadius: 25,
+    backgroundColor: PrimaryColor.Main,
+  },
+  buttonErrorStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 150,
+    height: 40,
+    borderRadius: 25,
+    borderWidth: 1,
+    borderColor: StatusColor.Error,
+    backgroundColor: BaseColor.White,
   },
 });
 
